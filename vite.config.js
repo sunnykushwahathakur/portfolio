1d82ba08972a4f5779e85ec1a81b9925d59fa00f
sunnykushwahathakur <sunnykushwahathakur@gmail.com>
2025-08-30 20:02:52 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve:{
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, './src'),
     }
+  },
+  build: {
+    rollupOptions: {
+      external: [] // Ensure no unnecessary external dependencies
+    }
   }
 })
-
